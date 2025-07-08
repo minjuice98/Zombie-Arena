73a746d76bf386c3371118656a875b712c49967e
윤준모 <ojm01088021296@gmail.com>
2025-07-08 15:42:15 +0900

    초기 세팅

---
+++ sfml-zombie/SceneGame.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "Scene.h"
+
+class Player;
+class Zombie;
+
+class SceneGame : public Scene
+{
+protected:
+	Player* player = nullptr;
+
+	std::list<Zombie*> zombieList;
+	std::list<Zombie*> zombiePool;
+
+	sf::Sprite cursor;
+
+public:
+	SceneGame();
+	~SceneGame() override = default;
+
+	void Init() override;
+	void Enter() override;
+	void Exit() override;
+	void Update(float dt) override;
+	void Draw(sf::RenderWindow& window) override;
+
+	void SpawnZombies(int count);
+
+	const std::list<Zombie*>& GetZombies() const 
+	{
+		return zombieList;
+	}
+};
+
